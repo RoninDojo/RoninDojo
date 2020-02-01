e6a691cc1fea2a48204010f594a992c5c7c82e4b
s2l1 <justalittlegist@protonmail.com>
2020-02-01 10:26:30 -0600

    Added change of ownership to user:user for backup drive

--- Scripts/Install/install-mount-backup-data-drive.sh
+++ Scripts/Install/install-mount-backup-data-drive.sh
@@ -89,6 +89,14 @@ sleep 2s
 
 echo -e "${RED}"
 echo "***"
+echo "Changing ownership of /mnt/usb1 to $USER:$USER..."
+echo "***"
+echo -e "${NC}"
+sudo chown -R $USER:$USER /mnt/usb1
+# use chown to change ownership to $USER [current user]
+
+echo -e "${RED}"
+echo "***"
 echo "Press any letter to return..."
 echo "***"
 echo -e "${NC}"
