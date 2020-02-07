a228b1f3b625d568ae31e9cfc5fcda600451d2e2
s2l1 <justalittlegist@protonmail.com>
2020-02-07 16:55:00 -0600

    changed format of ronin upgrade, added indication of completion, removed redundant code from ronin main menu

--- Scripts/Menu/menu-system2.sh
+++ Scripts/Menu/menu-system2.sh
@@ -50,21 +50,38 @@ case $CHOICE in
             # uses passwd to unlock root user, returns to menu
             ;;
         3)
+            sudo rm -f ~/ronin-update.sh
+	    # using -f here to avoid error output if ~/ronin-update.sh does not exist
+
             echo -e "${RED}"
             echo "***"
             echo "Upgrading Ronin..."
             echo "***"
             echo -e "${NC}"
             sleep 2s
+
             echo "sudo rm -rf ~/RoninDojo" > ~/ronin-update.sh
-            echo "sudo rm -rf /usr/local/bin/ronin" > ~/ronin-update.sh
+            echo "sudo rm -rf /usr/local/bin/ronin" >> ~/ronin-update.sh
             echo "cd ~" >> ~/ronin-update.sh
             echo "git clone https://github.com/RoninDojo/RoninDojo.git" >> ~/ronin-update.sh
-            echo "sudo cp ~/RoninDojo/ronin /usr/local/bin"
+            echo "sudo cp -rv ~/RoninDojo/ronin /usr/local/bin" >> ~/ronin-update.sh
+            # removes RoninDojo directory and ronin main menu script
+	    # changes to home directory, clones RoninDojo master branch, and copies new ronin menu to /user/local/bin
+
+            echo "echo -e '${RED}'" >> ~/ronin-update.sh
+            echo "echo '***'" >> ~/ronin-update.sh
+            echo "echo "Upgrade Complete!"" >> ~/ronin-update.sh
+            echo "echo '***'" >> ~/ronin-update.sh
+            echo "echo -e '${NC}'" >> ~/ronin-update.sh
+	    # notifies upgrade is complete
+
+	    echo "sleep 2s" >> ~/ronin-update.sh
             echo "bash ~/RoninDojo/Scripts/Menu/menu-system2.sh" >> ~/ronin-update.sh
             sudo chmod +x ~/ronin-update.sh
             bash ~/ronin-update.sh
-            # returns to menu
+            # makes script executable and runs
+	    # end of script returns to menu
+            # script is deleted during next run of update
             ;;
         4)
             bash ~/RoninDojo/Scripts/Install/install-mount-backup-data-drive.sh
