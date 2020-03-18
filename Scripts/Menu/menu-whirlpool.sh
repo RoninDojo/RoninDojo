427a7f1a97af6df1e3711354ab01f77b00487ec8
RoninDojo <19-Ronin@users.noreply.code.samourai.io>
2020-03-18 22:50:28 +0000

    enhanced whirlpool status option

--- Scripts/Menu/menu-whirlpool.sh
+++ Scripts/Menu/menu-whirlpool.sh
@@ -79,7 +79,7 @@ case $CHOICE in
             echo "Press Ctrl + C or q to exit at anytime..."
             echo "***"
             echo -e "${NC}"
-            sudo systemctl status whirlpool
+            sudo watch -n 0.2 systemctl status whirlpool —lines=10
             bash ~/RoninDojo/Scripts/Menu/menu-whirlpool.sh
             # view status, return to menu
             ;;
