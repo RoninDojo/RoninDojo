aac4733c95ccb456ca407235bb9534d8278add47
Fernando Leon Reyes (likewhoa) <likewhoa@weboperative.com>
2020-05-04 06:40:19 -0400

    Minor syntax fixes

--- Scripts/Install/install-dojo.sh
+++ Scripts/Install/install-dojo.sh
@@ -264,7 +264,7 @@ read -p "Do you want to install an indexer? [y/n]" yn
 case $yn in
     [Y/y]* )
       sudo sed -i 's/INDEXER_INSTALL=off/INDEXER_INSTALL=on/' ~/dojo/docker/my-dojo/conf/docker-indexer.conf.tpl
-      sudo -i 's/NODE_ACTIVE_INDEXER=local_bitcoind/NODE_ACTIVE_INDEXER=local_indexer' ~/dojo/docker/my-dojo/conf/docker-node.conf.tpl
+      sudo sed -i 's/NODE_ACTIVE_INDEXER=local_bitcoind/NODE_ACTIVE_INDEXER=local_indexer' ~/dojo/docker/my-dojo/conf/docker-node.conf.tpl
     [N/n]* ) echo "Indexer will not be installed!";;
     * ) echo "Please answer Yes or No.";;
 esac
