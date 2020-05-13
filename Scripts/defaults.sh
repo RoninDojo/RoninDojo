10aab8f48f61f0ffd65facc34804386fc9697636
likewhoa <likewhoa@weboperative.com>
2020-05-12 23:06:47 -0400

    Add some logic to defaults.sh

--- Scripts/defaults.sh
+++ Scripts/defaults.sh
@@ -6,7 +6,7 @@ YELLOW=$(tput setaf 3)
 NC=$(tput sgr0)
 # No Color
 
-DOJO_PATH='~/dojo/docker/my-dojo'
+DOJO_PATH=~/dojo/docker/my-dojo
 SAMOURAI_REPO='https://code.samourai.io/ronindojo/samourai-dojo.git'
 USER=$(getent group 1000 | cut -d ':' -f1)
 
@@ -18,6 +18,14 @@ WIDTH=76
 CHOICE_HEIGHT=16
 TITLE="RoninDojo"
 MENU="Choose one of the following options:"
+
+#
+# Dojo Existing Configuration Values
+#
+if [ -f ~/dojo/docker/my-dojo/conf/docker-node.conf ]; then
+    NODE_API_KEY_TOR=$(grep NODE_API_KEY ~/dojo/docker/my-dojo/conf/docker-node.conf | cut -d '=' -f2)
+    NODE_ADMIN_KEY_TOR=$(grep NODE_ADMIN_KEY ~/dojo/docker/my-dojo/conf/docker-node.conf | cut -d '=' -f2)
+fi
 
 #
 # Dojo Docker settings
@@ -43,14 +51,30 @@ MYSQL_PASSWORD=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 64 | head -n 1)
 EXPLORER_KEY=$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 16 | head -n 1)
 EXPLORER_KEY_TOR=$(grep EXPLORER_KEY ~/dojo/docker/my-dojo/conf/docker-explorer.conf | cut -d '=' -f2)
 
-#Tor Hiddenservice
-V3_ADDR_API=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3dojo/hostname)
-NODE_API_KEY_TOR=$(grep NODE_API_KEY ~/dojo/docker/my-dojo/conf/docker-node.conf | cut -d '=' -f2)
-NODE_ADMIN_KEY_TOR=$(grep NODE_ADMIN_KEY ~/dojo/docker/my-dojo/conf/docker-node.conf | cut -d '=' -f2)
-V3_ADDR_WHIRLPOOL=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3whirlpool/hostname)
-WHIRLPOOL_API_KEY=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-whirlpool/_data/.whirlpool-cli/whirlpool-cli-config.properties | grep cli.apiKey= | cut -c 12-)
-V3_ADDR_EXPLORER=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3explorer/hostname)
-V3_ADDR_ELECTRS=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3electrs/hostname)
+#
+# Tor Hidden Service Addresses
+#
+
+# Dojo Maintanance Tool
+if [ -d /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3dojo ]; then
+    V3_ADDR_API=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3dojo/hostname)
+fi
+
+# Whirlpool
+if [ -d /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3whirlpool ]; then
+    V3_ADDR_WHIRLPOOL=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3whirlpool/hostname)
+    WHIRLPOOL_API_KEY=$(sudo grep cli.apiKey /mnt/usb/docker/volumes/my-dojo_data-whirlpool/_data/.whirlpool-cli/whirlpool-cli-config.properties | cut -d '=' -f2)
+fi
+
+# Explorer
+if [ -d /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3explorer ]; then
+    V3_ADDR_EXPLORER=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3explorer/hostname)
+fi
+
+# Electrum Server
+if [ -d /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3electrs [; then
+    V3_ADDR_ELECTRS=$(sudo cat /mnt/usb/docker/volumes/my-dojo_data-tor/_data/hsv3electrs/hostname)
+fi
 
 # Ronin menu paths
 RONIN_DOJO_MENU='~/RoninDojo/Scripts/Menu/menu-dojo.sh'
