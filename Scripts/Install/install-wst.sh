226947daf1c86709d320734581a8e9e13a0b1a5c
likewhoa <likewhoa@weboperative.com>
2020-05-12 23:39:42 -0400

    Tweak whirlpool stats install

--- Scripts/Install/install-wst.sh
+++ Scripts/Install/install-wst.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 . ~/RoninDojo/Scripts/defaults.sh
+. ~/RoninDojo/Scripts/functions.sh
 
 echo -e "${RED}"
 echo "***"
 echo "Installing Whirlpool Stat Tool..."
 echo "***"
 echo -e "${NC}"
-mkdir ~/wst;
-cd ~/wst;
+
+mkdir ~/wst && cd ~/wst
+
 git clone https://github.com/Samourai-Wallet/whirlpool_stats.git;
-sudo pacman -Syyu;
-sudo pacman -S python-pip;
-cd whirlpool_stats;
-sudo pip3 install -r ./requirements.txt;
+
+if find_pkg python-pip; then
+  echo -e "${RED}"
+  echo "***"
+  echo "python-pip already installed..."
+  echo "***"
+  echo -e "${NC}"
+  sleep 1s
+else
+  echo -e "${RED}"
+  echo "***"
+  echo "Installing python-pip..."
+  echo "***"
+  echo -e "${NC}"
+  sleep 1s
+  sudo pacman -S --noconfirm python-pip
+fi
+# check for / python-pip
+
+cd whirlpool_stats
+sudo pip3 install -r ./requirements.txt
+
 bash ~/RoninDojo/Scripts/Menu/menu-whirlpool-wst.sh
