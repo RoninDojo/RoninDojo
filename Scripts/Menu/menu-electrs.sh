471eb25d9e4dce9c3d8d7b2c5b8de4943e119e77
BTCxZelko <btcxzelko@protonmail.com>
2020-05-16 21:20:50 +0000

    fixed L52

--- Scripts/Menu/menu-electrs.sh
+++ Scripts/Menu/menu-electrs.sh
@@ -49,7 +49,7 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             sleep 2s
-            cd $DOJO_PATH && ./dojo.sh indexer
+            cd $DOJO_PATH && ./dojo.sh logs indexer
             bash ~/RoninDojo/Scripts/Menu/menu-electrs.sh
             # start electrs, return to menu
             ;;
