f838c63bb59faecad77888907477e9b8dd53a421
s2l1 <s2l1@pm.me>
2020-05-29 00:57:30 +0000

    changed to match format of regular logs page and now need to test and clean up troubleshoot logs output before adding back to menu

--- Scripts/Menu/menu-dojo-error-logs.sh
+++ Scripts/Menu/menu-dojo-error-logs.sh
@@ -6,11 +6,12 @@
 
 OPTIONS=(1 "Bitcoind Logs"
          2 "Db Logs"
-         3 "Tor Logs"
-         4 "API Logs"
-         5 "Tracker Logs"
-         6 "All Container Logs"
-         7 "Go Back")
+         3 "Indexer Logs"
+         4 "Nginx Logs"
+         5 "Nodejs Logs"
+         6 "Tor Logs"
+         7 "Explorer Logs"
+         8 "Go Back")
 
 CHOICE=$(dialog --clear \
                 --title "$TITLE" \
@@ -41,9 +42,10 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             sleep 2s
-            cd $DOJO_PATH && ./dojo.sh logs bitcoind -n 200
+            cd "$DOJO_PATH" || exit 
+            ./dojo.sh logs bitcoind -n 200
             bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
-            # shows dojo bitcoind error logs
+            # shows bitcoind error logs
             ;;
         2)
             isRunning=$(docker inspect --format="{{.State.Running}}" db 2> /dev/null)
@@ -65,9 +67,10 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             sleep 2s
-            cd $DOJO_PATH && ./dojo.sh logs db -n 500 | grep error
+            cd "$DOJO_PATH" || exit
+            ./dojo.sh logs db -n 500 | grep error
             bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
-            # shows dojo db error logs
+            # shows db error logs
             ;;
         3)
             isRunning=$(docker inspect --format="{{.State.Running}}" db 2> /dev/null)
@@ -89,9 +92,10 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             sleep 2s
-            cd $DOJO_PATH && ./dojo.sh logs tor -n 500
+            cd "$DOJO_PATH" || exit
+            ./dojo.sh logs indexer -n 500
             bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
-            # shows dojo tor error logs
+            # shows tor error logs
             ;;
         4)
             isRunning=$(docker inspect --format="{{.State.Running}}" db 2> /dev/null)
@@ -113,9 +117,10 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             sleep 2s
-            cd $DOJO_PATH && ./dojo.sh logs node -n 500 | grep HttpServer
+            cd "$DOJO_PATH" || exit
+            ./dojo.sh logs nginx -n 500 | grep HttpServer
             bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
-            # shows dojo api error logs
+            # shows nginx error logs
             ;;
         5)
             isRunning=$(docker inspect --format="{{.State.Running}}" db 2> /dev/null)
@@ -137,9 +142,10 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             sleep 2s
-            cd $DOJO_PATH && ./dojo.sh logs node -n 500 | grep Tracker
+            cd "$DOJO_PATH" || exit
+            ./dojo.sh logs nodejs -n 500 | grep Tracker
             bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
-            # shows dojo tracker error logs
+            # shows nodejs error logs
             ;;
         6)
             isRunning=$(docker inspect --format="{{.State.Running}}" db 2> /dev/null)
@@ -161,16 +167,35 @@ case $CHOICE in
             echo "***"
             echo -e "${NC}"
             _sleep 2
+            cd "$DOJO_PATH" || exit
+            ./dojo.sh logs tor -n 500
+            bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
+            # shows tor error logs
+            ;;
+        7)
+            isRunning=$(docker inspect --format="{{.State.Running}}" db 2> /dev/null)
+            if [ $? -eq 1 ] || [ "$isRunning" == "false" ]; then
+              echo -e "${RED}"
+              echo "***"
+              echo "Dojo needs to be started first!"
+              echo "***"
+              echo -e "${NC}"
+              _sleep 5
+              bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
+              exit
+            fi
+            # checks if dojo is running (check the db container), if not running tells user to start dojo first
 
             echo -e "${RED}"
             echo "***"
-            echo "This command may take some time."
+            echo "Press Ctrl + C to exit at any time."
             echo "***"
             echo -e "${NC}"
-            sleep 1s
-            cd $DOJO_PATH && ./dojo.sh logs whirlpool -n 500 | grep Error
+            _sleep 2
+            cd "$DOJO_PATH" || exit
+            ./dojo.sh logs explorer -n 500
             bash ~/RoninDojo/Scripts/Menu/menu-dojo-error-logs.sh
-            # shows whirlpool docker container error logs
+            # shows explorer error logs
             ;;
         8)
             bash -c "$RONIN_DOJO_MENU"
