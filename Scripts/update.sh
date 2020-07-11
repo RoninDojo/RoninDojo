f8f8144b3216130c61eed280cfe945628bd76db2
likewhoa <likewhoa@weboperative.com>
2020-07-11 14:43:16 -0400

    Remove use of functions.sh _sleep() to prevent loop

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # shellcheck source=/dev/null
 
-. "$HOME"/RoninDojo/Scripts/functions.sh
-
 _update_01() {
     if ! _check_pkgver bridge-utils 1.7-1; then
         cat <<EOF
@@ -13,7 +11,14 @@ RoninDojo will upgade your package to latest version available
 ***
 ${NC}
 EOF
-        _sleep 5 --msg "Starting bridge-utils upgrade in"
+        sleep 2
+        cat <<EOF
+${RED}
+***
+Starting bridge-utils upgrade...
+***
+${NC}
+EOF
         sudo pacman -U --noconfirm http://mirror.archlinuxarm.org/aarch64/extra/bridge-utils-1.7-1-aarch64.pkg.tar.xz &>/dev/null
 
         # If existing dojo found, then reboot system to apply changes
