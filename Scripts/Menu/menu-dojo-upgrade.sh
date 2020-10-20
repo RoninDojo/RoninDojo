5caf73a7ab70695b48c4c0d4b3ae6f452c7eedc0
s2l1 <s2l1@pm.me>
2020-10-19 20:25:23 -0500

    Indexer install prompt for menu-dojo-upgrade.sh

--- Scripts/Menu/menu-dojo-upgrade.sh
+++ Scripts/Menu/menu-dojo-upgrade.sh
@@ -42,9 +42,10 @@ _check_dojo_perms "${DOJO_PATH}"
 if grep BITCOIND_RPC_EXTERNAL=off "${DOJO_PATH}"/conf/docker-bitcoind.conf 1>/dev/null; then
     sed -i 's/BITCOIND_RPC_EXTERNAL=off/BITCOIND_RPC_EXTERNAL=on/' "${DOJO_PATH}"/conf/docker-bitcoind.conf
 fi
-# enable BITCOIND_RPC_EXTERNAL
+
 cd "${WORK_DIR}" || exit
 git clone -b "${SAMOURAI_COMMITISH:-master}" "$SAMOURAI_REPO" 2>/dev/null # temporary
+# enable BITCOIND_RPC_EXTERNAL
 
 cp -ua samourai-dojo/* "${DOJO_PATH%/docker/my-dojo}"/
 # copy only when the SOURCE file is newer than the
@@ -54,8 +55,8 @@ cp -ua samourai-dojo/* "${DOJO_PATH%/docker/my-dojo}"/
 rm -rf "${WORK_DIR}"
 # remove $WORK_DIR
 
-# Return to previous working path
 cd "${HOME}" || exit
+# return to previous working path
 
         cat <<EOF
 ${RED}
@@ -74,6 +75,7 @@ BTC RPC Explorer is already installed!
 ***
 ${NC}
 EOF
+
 else
     cat <<EOF
 ${RED}
@@ -83,6 +85,7 @@ Installing your BTC RPC Explorer...
 ${NC}
 EOF
     _sleep 2
+    
     cat <<EOF
 ${RED}
 ***
@@ -96,16 +99,130 @@ fi
 # checks for docker-explorer.conf, if found informs user
 # else uses sed to modify for explorer to be installed
 
+if grep "INDEXER_INSTALL=off" "${DOJO_PATH}"/conf/docker-indexer.conf 1>/dev/null && [ ! -f "${DOJO_PATH}"/indexer/electrs.toml ] ; then
     cat <<EOF
 ${RED}
 ***
-Checking Indexer...
+Checking for Indexer...
+***
+${NC}
+EOF
+_sleep 1
+# if indexer and electrs are not found then give user menu for install choices
+
+    cat <<EOF
+${RED}
+***
+No Indexer found...
+***
+${NC}
+EOF
+_sleep 1
+
+    cat <<EOF
+${RED}
+***
+Preparing for Indexer Prompt...
+***
+${NC}
+EOF
+_sleep 2
+
+    cat <<EOF
+${RED}
+***
+Samourai Indexer is recommended for most users as it helps with querying balances...
+***
+${NC}
+EOF
+_sleep 3
+
+    cat <<EOF
+${RED}
+***
+Electrum Rust Server is recommended for Hardware Wallets, Multisig, and other Electrum features...
+***
+${NC}
+EOF
+_sleep 3
+
+    cat <<EOF
+${RED}
+***
+Choose one of the following options for your Indexer...
+***
+${NC}
+EOF
+_sleep 3
+
+select indexer in "Samourai Indexer" "Electrum Rust Server" "Do Not Install Indexer"
+# indexer names here are used as data source
+
+do
+case $indexer in
+"Samourai Indexer")
+    cat <<EOF
+${RED}
+***
+Installing Samourai Indexer...
+***
+${NC}
+EOF
+_sleep
+sudo sed -i 's/INDEXER_INSTALL=off/INDEXER_INSTALL=on/' "${DOJO_PATH}"/conf/docker-indexer.conf
+sudo sed -i 's/NODE_ACTIVE_INDEXER=local_bitcoind/NODE_ACTIVE_INDEXER=local_indexer/' "${DOJO_PATH}"/conf/docker-node.conf
+break;;
+# samourai indexer install enabled in .conf.tpl files using sed
+
+"Electrum Rust Server")
+    cat <<EOF
+${RED}
+***
+Installing Electrum Rust Server...
+***
+${NC}
+EOF
+_sleep
+bash "$HOME"/RoninDojo/Scripts/Menu/menu-dojo-electrs-upgrade.sh;;
+# triggers electrs install script
+
+"Do Not Install Indexer")
+    cat <<EOF
+${RED}
+***
+An Indexer will not be installed during this upgrade...
+***
+${NC}
+EOF
+_sleep
+break;;
+# indexer will not be installed
+
+*)
+    cat <<EOF
+${RED}
+***
+Invalid Entry!
+***
+${NC}
+EOF
+_sleep
+;;
+# invalid data try again
+esac
+done
+
+else
+    cat <<EOF
+${RED}
+***
+Checking for Indexer...
 ***
 ${NC}
 EOF
     _sleep 1
 
-if grep "INDEXER_INSTALL=off" "${DOJO_PATH}"/conf/docker-indexer.conf 1>/dev/null; then
+    if grep "INDEXER_INSTALL=off" "${DOJO_PATH}"/conf/docker-indexer.conf 1>/dev/null; then
         cat <<EOF
 ${RED}
 ***
@@ -113,93 +230,128 @@ No Indexer found...
 ***
 ${NC}
 EOF
-    _sleep 3
+        _sleep 3
+        
         cat <<EOF
 ${RED}
 ***
-If you want an Indexer press "Y" when prompted, this is recommended for better performance...
+If you want to install Samourai Indexer press "Y" when prompted...
+***
+${NC}
+
+${RED}
+***
+This is recommended for most users as it helps with querying balances...
 ***
 ${NC}
 EOF
-    _sleep 5
-    read -rp "Do you want to install an Indexer? [y/n]" yn
-    case $yn in
-        [Y/y]* )
-                 sudo sed -i 's/INDEXER_INSTALL=off/INDEXER_INSTALL=on/' "${DOJO_PATH}"/conf/docker-indexer.conf
-                 sudo sed -i 's/NODE_ACTIVE_INDEXER=local_bitcoind/NODE_ACTIVE_INDEXER=local_indexer/' "${DOJO_PATH}"/conf/docker-node.conf;;
-        [N/n]* ) cat <<EOF
-                 ${RED}
-                 ***
-                 Indexer will not be installed...
-                 ***
-                 ${NC}
+        _sleep 5
+        read -rp "Do you want to install an Indexer? [y/n]" yn
+        case $yn in
+            [Y/y]* )
+                     sudo sed -i 's/INDEXER_INSTALL=off/INDEXER_INSTALL=on/' "${DOJO_PATH}"/conf/docker-indexer.conf
+                     sudo sed -i 's/NODE_ACTIVE_INDEXER=local_bitcoind/NODE_ACTIVE_INDEXER=local_indexer/' "${DOJO_PATH}"/conf/docker-node.conf;;
+            [N/n]* ) cat <<EOF
+                     ${RED}
+                     ***
+                     Indexer will not be installed...
+                     ***
+                     ${NC}
 EOF;;
-        * ) printf "\nPlease answer Yes or No.\n";;
-    esac
-else
-    cat <<EOF
+            * ) printf "\nPlease answer Yes or No.\n";;
+        esac
+    
+    else
+        cat <<EOF
 ${RED}
 ***
 Indexer is already installed...
 ***
 ${NC}
 EOF
-fi
-# if docker-indexer.conf is not found prompt user to select
-# for elif, if grep search INDEXER_INSTALL=off works, prompt user
-# else informs user indexer is already installed
+    fi
+        # if docker-indexer.conf is not found prompt user to select
+        # for elif, if grep search INDEXER_INSTALL=off works, prompt user
+        # else informs user indexer is already installed
 
     cat <<EOF
 ${RED}
 ***
-Checking Electrs...
+Checking for Electrum Rust Server...
 ***
 ${NC}
 EOF
     _sleep 1
 
-if [ ! -f "${DOJO_PATH}"/indexer/electrs.toml ] ; then
-       cat <<EOF
+    if [ ! -f "${DOJO_PATH}"/indexer/electrs.toml ] ; then
+        cat <<EOF
 ${RED}
 ***
-No Electrs found...
+No Electrum Rust Server found...
 ***
 ${NC}
 EOF
-    _sleep 3
+       _sleep 3
+
        cat <<EOF
 ${RED}
 ***
-If you want Electrs press "Y" when prompted, this is for Electrum and Hardware Wallets...
+If you want to install Electrum Rust Server press "Y" when prompted...
+***
+${NC}
+
+${RED}
+***
+Electrum Rust Server is recommended for Hardware Wallets, Multisig, and other Electrum features...
 ***
 ${NC}
 EOF
-    _sleep 3
-   read -rp "Do you want to install Electrs? [y/n]" yn
-   case $yn in
-       [Y/y]* ) bash "$HOME"/RoninDojo/Scripts/Menu/menu-dojo-electrs-upgrade.sh;;
-       [N/n]* ) cat <<EOF
-                ${RED}
-                ***
-                Electrs will not be installed!
-                ***
-                ${NC}
+       _sleep 5
+       read -rp "Do you want to install Electrum Rust Server? [y/n]" yn
+       case $yn in
+           [Y/y]* ) bash "$HOME"/RoninDojo/Scripts/Menu/menu-dojo-electrs-upgrade.sh;;
+           [N/n]* ) cat <<EOF
+                    ${RED}
+                    ***
+                    Electrum Rust Server will not be installed!
+                    ***
+                    ${NC}
 EOF;;
-       * ) printf "\nPlease answer Yes or No.\n";;
-    esac
-else
-   cat <<EOF
+           * ) printf "\nPlease answer Yes or No.\n";;
+        esac
+        
+    else
+        cat <<EOF
    ${RED}
    ***
-   Electrs is already installed!
+   Electrum Rust Server is already installed!
    ***
    ${NC}
 EOF
-   _sleep 3
-   bash "$HOME"/RoninDojo/Scripts/Menu/menu-dojo-electrs-upgrade.sh
-fi
+        _sleep 3
+        bash "$HOME"/RoninDojo/Scripts/Menu/menu-dojo-electrs-upgrade.sh
+    fi
 # if electrs.toml is not found the user is prompted to select y/n
 # else informs user indexer is already installed
+fi      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if [ -f /etc/systemd/system/whirlpool.service ] ; then
    sudo systemctl stop whirlpool
