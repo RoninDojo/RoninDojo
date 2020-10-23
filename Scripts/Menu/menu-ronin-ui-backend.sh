RoninDojo/RoninDojo

commit 6b5a1bef26c9482c235276b8b824b1e758186541

fix some menu bugs, added menu option for user to change password, added press any letter to return to a few places to eliminate confusion, fixed text outputs, renamed a few things, and general code cleanup

Scripts/Menu/menu-ronin-ui-backend.sh
@@ -27,7 +27,7 @@ case $CHOICE in
 	1)
         # Check if process running, otherwise start it
         if pm2 describe "Ronin Backend" | grep status | grep stopped 1>/dev/null; then
-            cat << EOF
+            
 ${RED}
 ***
 Starting UI Backend Server...
@@ -39,7 +39,7 @@ EOF
 
             pm2 start "Ronin Backend"
         else
-            cat << EOF
+            
 ${RED}
 ***
 UI Backend already started...
@@ -48,6 +48,16 @@ ${NC}
 EOF
             _sleep 2
         fi
+        
+        
+${RED}
+***
+Press any letter to return...
+***
+${NC}
+EOF
+        read -n 1 -r -s
+        # press to return is needed so the user has time to see outputs
 
         bash -c "${HOME}"/RoninDojo/Scripts/Menu/menu-ronin-ui-backend.sh
         # start Ronin UI Backend, return to menu
@@ -55,7 +65,7 @@ EOF
     2)
         # Check if process running before stopping it
         if pm2 describe "Ronin Backend" &>/dev/null; then
-            cat << EOF
+            
 ${RED}
 ***
 Stopping UI Backend Server...
@@ -67,7 +77,7 @@ EOF
 
             pm2 stop "Ronin Backend"
         else
-            cat << EOF
+            
 ${RED}
 ***
 UI Backend Server already stopped...
@@ -76,11 +86,21 @@ ${NC}
 EOF
         fi
 
+
+${RED}
+***
+Press any letter to return...
+***
+${NC}
+EOF
+        read -n 1 -r -s
+        # press to return is needed so the user has time to see outputs
+
         bash -c "${HOME}"/RoninDojo/Scripts/Menu/menu-ronin-ui-backend.sh
         # start Ronin UI Backend, return to menu
         ;;
     3)
-        cat << EOF
+        
 ${RED}
 ***
 Restarting UI Backend Server...
@@ -90,17 +110,29 @@ EOF
         _sleep 2
         cd "${RONIN_UI_BACKEND_DIR}" || exit
 
-        # Restart service
+        
         pm2 restart "Ronin Backend" 1>/dev/null
+        # restart service
+
+        
+${RED}
+***
+Press any letter to return...
+***
+${NC}
+EOF
+
+        read -n 1 -r -s
+        # press to return is needed so the user has time to see outputs
 
         bash -c "${HOME}"/RoninDojo/Scripts/Menu/menu-ronin-ui-backend.sh
         # start Ronin UI Backend, return to menu
         ;;
     4)
-        cat << EOF
+        
 ${RED}
 ***
-Press any key to return.
+Showing UI Backend Status...
 ***
 ${NC}
 EOF
@@ -111,10 +143,10 @@ EOF
         bash -c "${HOME}"/RoninDojo/Scripts/Menu/menu-ronin-ui-backend.sh
         ;;
     5)
-        cat << EOF
+        
 ${RED}
 ***
-Press q key to exit at any time.
+Press "q" key to exit at any time...
 ***
 ${NC}
 EOF
@@ -125,10 +157,13 @@ EOF
         bash -c "${HOME}"/RoninDojo/Scripts/Menu/menu-ronin-ui-backend.sh
         ;;
     6)
-        cat << EOF
+        cat <<EOF
 ${RED}
 ***
 Installing Ronin UI Backend...
+***
+
+***
 Press Ctrl+C to cancel at anytime
 ***
 ${NC}
@@ -144,10 +179,13 @@ EOF
     7)
         cd "${RONIN_UI_BACKEND_DIR}" || exit
 
-        cat << EOF
+        cat <<EOF
 ${RED}
 ***
 Uninstalling Ronin UI Backend...
+***
+
+***
 Press Ctrl+C to cancel at anytime
 ***
 ${NC}

Scripts/.logo
@@ -5,7 +5,7 @@ RED=$(tput setaf 1)
 NC=$(tput sgr0)
 # No Color
 
-cat << EOF
+cat <<EOF
 ${NC}
 _____________________________________________________|_._._._._._._._._,
 \____________________________________________________|█|█|█|█|█|█|█|█|█|

Scripts/Install/install-boltzmann.sh
@@ -52,7 +52,7 @@ pipenv install -r requirements.txt &>/dev/null
 
 echo -e "${RED}"
 echo "***"
-echo "Press any letter to continue..."
+echo "Press any letter to return..."
 echo "***"
 echo -e "${NC}"
 read -n 1 -r -s

Scripts/Install/install-dojo.sh
@@ -11,8 +11,10 @@ if ! findmnt "${INSTALL_DIR}" 1>/dev/null; then
   cat <<DOJO
 ${RED}
 ***
-Missing drive mount at ${INSTALL_DIR}!
-Please contact support for assistance.
+Missing drive mount at ${INSTALL_DIR}! Please contact support for assistance.
+***
+
+***
 Exiting RoninDojo in 5 seconds...
 ***
 ${NC}
@@ -57,26 +59,25 @@ git clone -b "${SAMOURAI_COMMITISH:-master}" "$SAMOURAI_REPO" dojo 2>/dev/null
 
 echo -e "${RED}"
 echo "***"
-echo "Values necessary for usernames, passwords, etc. will randomly be generated now..."
+echo "Credentials necessary for usernames, passwords, etc. will randomly be generated now..."
 echo "***"
 echo -e "${NC}"
 
 cat <<DOJO
 ${RED}
 ***
-These values are found in RoninDojo menus, ${DOJO_PATH}/conf directory
-or in the ~/RoninDojo/user.conf.example file. See file for more info
+Credentials are found in RoninDojo menu, ${DOJO_PATH}/conf, or in the ~/RoninDojo/user.conf.example file.
 ***
 ${NC}
 DOJO
-_sleep
+_sleep 2
 
 echo -e "${RED}"
 echo "***"
-echo "Be aware you will use these values to login to Dojo Maintenance Tool, Block Explorer, and more!"
+echo "Be aware these credentials are used to login to Dojo Maintenance Tool, Block Explorer, and more!"
 echo "***"
 echo -e "${NC}"
-_sleep 2
+_sleep 5
 
 echo -e "${RED}"
 echo "***"

Scripts/Install/install-mount-backup-data-drive.sh
@@ -28,7 +28,7 @@ else
   echo "***"
   echo -e "${NC}"
   read -n 1 -r -s
-  bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh
+  bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh
   # no drive detected, press any letter to return to menu
 fi
 
@@ -70,5 +70,5 @@ echo "Press any letter to return..."
 echo "***"
 echo -e "${NC}"
 read -n 1 -r -s
-bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh
-# press any letter to return to menu-system2.sh
+bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh
+# press any letter to return to menu-system-storage.sh

Scripts/Install/install-new-backup-data-drive.sh
@@ -28,7 +28,7 @@ else
   echo "***"
   echo -e "${NC}"
   read -n 1 -r -s
-  bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh
+  bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh
   # no drive detected, press any letter to return to menu
 fi
 
@@ -53,7 +53,7 @@ while true; do
     read -rp "Y/N?: " yn
     case $yn in
         [Yy]* ) break;;
-        [Nn]* ) bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh;exit;;
+        [Nn]* ) bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh;exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -96,5 +96,5 @@ echo "Press any letter to return..."
 echo "***"
 echo -e "${NC}"
 read -n 1 -r -s
-bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh
-# press any letter to return to menu-system2.sh
+bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh
+# press any letter to return to menu-system-storage.sh

Scripts/Install/install-umount-backup-data-drive.sh
@@ -28,7 +28,7 @@ else
   echo "***"
   echo -e "${NC}"
   read -n 1 -r -s
-  bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh
+  bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh
   # no drive detected, press any letter to return to menu
 fi
 
@@ -67,5 +67,5 @@ echo "Press any letter to return..."
 echo "***"
 echo -e "${NC}"
 read -n 1 -r -s
-bash "$HOME"/RoninDojo/Scripts/Menu/menu-system2.sh
-# press any letter to return to menu-system2.sh
+bash "$HOME"/RoninDojo/Scripts/Menu/menu-system-storage.sh
+# press any letter to return to menu-system-storage.sh

Scripts/Menu/menu-credentials.sh
@@ -27,18 +27,20 @@ case $CHOICE in
         cat <<MENU
 ${RED}
 ***
-${NC}
 Samourai Dojo Credentials
-${RED}
-WARNING: Do not share these onion addresses with anyone!
-         To allow another person to use this Dojo with her Samourai Wallet,
-         you should share the QRCodes provided by the Maintenance Tool.
-
 ***
 ${NC}
+
+${RED}
+***
+WARNING: Do not share these onion addresses with anyone!
+***
+${NC}
+
 Maintenance Tool            = http://$V3_ADDR_API/admin
 Admin Key                   = $NODE_ADMIN_KEY
 API Key                     = $NODE_API_KEY
+
 ${RED}
 ***
 Press any letter to return...
@@ -54,17 +56,19 @@ MENU
         cat <<MENU
 ${RED}
 ***
-${NC}
 Samourai Whirlpool Credentials
-${RED}
-WARNING: Do not share these onion addresses with anyone!
-        To allow another person to use this Dojo with her Samourai Wallet,
-        you should share the QRCodes provided by the Maintenance Tool.
-
 ***
 ${NC}
+
+${RED}
+***
+WARNING: Do not share these onion addresses with anyone!
+***
+${NC}
+
 Whirlpool Tor URL           = http://$V3_ADDR_WHIRLPOOL
 Whirlpool API Key           = ${WHIRLPOOL_API_KEY:-Whirlpool not Initiated yet. Pair wallet with GUI}
+
 ${RED}
 ***
 Press any letter to return...
@@ -102,6 +106,7 @@ ${RED}
 Electrs Credentials
 ***
 ${NC}
+
 Electrs Tor URL = $V3_ADDR_ELECTRS
 MENU
         _sleep
@@ -112,6 +117,7 @@ ${RED}
 ***
 Check the RoninDojo Wiki for pairing information at https://wiki.ronindojo.io
 ***
+
 ${RED}
 ***
 Press any letter to return...
@@ -156,11 +162,10 @@ MENU
         cat <<MENU
 ${RED}
 ***
-${NC}
 Bitcoin Credentials
-${RED}
 ***
 ${NC}
+
 Bitcoin Daemon:
 
 Tor URL                 = http://$V2_ADDR_BITCOIN
@@ -173,6 +178,7 @@ Bitcoin RPC Explorer:
 
 Tor URL                 = http://$V3_ADDR_EXPLORER (No username required)
 Password                = $EXPLORER_KEY
+
 ${RED}
 ***
 Press any letter to return...

commit 9fe43dd20a150e5e1f15370edf3b19a59106a0d6

Fix bug introduced in 6b5a1bef

Scripts/Menu/menu-ronin-ui-backend.sh
@@ -27,7 +27,7 @@ case $CHOICE in
 	1)
         # Check if process running, otherwise start it
         if pm2 describe "Ronin Backend" | grep status | grep stopped 1>/dev/null; then
-            
+            cat <<EOF
 ${RED}
 ***
 Starting UI Backend Server...
@@ -39,7 +39,7 @@ EOF
 
             pm2 start "Ronin Backend"
         else
-            
+            cat <<EOF
 ${RED}
 ***
 UI Backend already started...
@@ -48,8 +48,8 @@ ${NC}
 EOF
             _sleep 2
         fi
-        
-        
+
+        cat <<EOF
 ${RED}
 ***
 Press any letter to return...
@@ -65,7 +65,7 @@ EOF
     2)
         # Check if process running before stopping it
         if pm2 describe "Ronin Backend" &>/dev/null; then
-            
+            cat <<EOF
 ${RED}
 ***
 Stopping UI Backend Server...
@@ -77,7 +77,7 @@ EOF
 
             pm2 stop "Ronin Backend"
         else
-            
+            cat <<EOF
 ${RED}
 ***
 UI Backend Server already stopped...
@@ -86,7 +86,7 @@ ${NC}
 EOF
         fi
 
-
+        cat <<EOF
 ${RED}
 ***
 Press any letter to return...
@@ -100,7 +100,7 @@ EOF
         # start Ronin UI Backend, return to menu
         ;;
     3)
-        
+        cat <<EOF
 ${RED}
 ***
 Restarting UI Backend Server...
@@ -110,11 +110,10 @@ EOF
         _sleep 2
         cd "${RONIN_UI_BACKEND_DIR}" || exit
 
-        
         pm2 restart "Ronin Backend" 1>/dev/null
         # restart service
 
-        
+        cat <<EOF
 ${RED}
 ***
 Press any letter to return...
@@ -129,7 +128,7 @@ EOF
         # start Ronin UI Backend, return to menu
         ;;
     4)
-        
+        cat <<EOF
 ${RED}
 ***
 Showing UI Backend Status...
@@ -143,7 +142,7 @@ EOF
         bash -c "${HOME}"/RoninDojo/Scripts/Menu/menu-ronin-ui-backend.sh
         ;;
     5)
-        
+        cat <<EOF
 ${RED}
 ***
 Press "q" key to exit at any time...