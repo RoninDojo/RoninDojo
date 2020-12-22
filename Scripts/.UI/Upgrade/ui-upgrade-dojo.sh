9fcf245fc805bb557833171ad467462338f9676e
BTCxZelko <btcxzelko@protonmail.com>
2020-12-22 14:09:32 -0800

    modify upgrade scripts to include indexer check

--- Scripts/.UI/Upgrade/ui-upgrade-dojo.sh
+++ Scripts/.UI/Upgrade/ui-upgrade-dojo.sh
@@ -18,12 +18,19 @@ fi
 _load_user_conf
 _check_dojo_perms "${dojo_path_my_dojo}"
 
+# Update Samourai Dojo repo
+_dojo_update
+
 if grep BITCOIND_RPC_EXTERNAL=off "${dojo_path_my_dojo}"/conf/docker-bitcoind.conf 1>/dev/null; then
     sed -i 's/BITCOIND_RPC_EXTERNAL=.*$/BITCOIND_RPC_EXTERNAL=on/' "${dojo_path_my_dojo}"/conf/docker-bitcoind.conf
 fi
 # enable BITCOIND_RPC_EXTERNAL
 
-# Update Samourai Dojo repo
-_dojo_update
+if grep "INDEXER_INSTALL=on" "${dojo_path_my_dojo}"/conf/docker-indexer.conf 1>/dev/null && [ -f "${dojo_path_my_dojo}"/indexer/electrs.toml ] ; then
+    bash "$HOME"/RoninDojo/Scripts/Install/install-electrs-indexer.sh
+else grep "INDEXER_INSTALL=on" "${dojo_path_my_dojo}"/conf/docker-indexer.conf 1>/dev/null && [ ! -f "${dojo_path_my_dojo}"/indexer/electrs.toml ] ; then
+    _set_addrindexer
+fi
 
-cd "${HOME}" || exit
+_is_mempool
+_mempool_urls_to_local_btc_explorer
