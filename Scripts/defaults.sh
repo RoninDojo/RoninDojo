bbc65690ff97af8a72c52e739561e5d8b442e6a4
likewhoa <likewhoa@weboperative.com>
2021-01-23 01:22:51 -0500

    Revert "Change RONIN_DOJO_BRANCH variable naming"
    This reverts commit fb04216fe28cc5f034d1da3d82b3ed0c6284afa3.

--- Scripts/defaults.sh
+++ Scripts/defaults.sh
@@ -80,7 +80,7 @@ DOJO_RESTORE=true
 TOR_RESTORE=true
 
 # Repositories
-RONIN_DOJO_BRANCH="origin/master" # defaults to master
+RONIN_DOJO_BRANCH="master" # defaults to master
 SAMOURAI_REPO='https://code.samourai.io/ronindojo/samourai-dojo.git'
 SAMOURAI_COMMITISH="v1.8.1" # empty defaults to master
 BOLTZMANN_REPO='https://code.samourai.io/oxt/boltzmann.git'
