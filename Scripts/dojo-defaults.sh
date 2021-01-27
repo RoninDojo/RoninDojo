eea145466684b25dee5f24868ff13c30dcce1a21
likewhoa <likewhoa@weboperative.com>
2021-01-27 03:48:25 -0500

    Fix bug when specter is not installed in a fresh environment

--- Scripts/dojo-defaults.sh
+++ Scripts/dojo-defaults.sh
@@ -60,6 +60,8 @@ if sudo test -d "${DOCKER_VOLUME_TOR}"/_data/hsv3mempool; then
 fi
 
 # Specter
+shopt -s nullglob
+
 for dir in "${HOME}"/specter*; do
     if [[ -d "${dir}" ]]; then
         V3_ADDR_SPECTER=$(sudo cat "${INSTALL_DIR_TOR}"/specter_server/hostname)
