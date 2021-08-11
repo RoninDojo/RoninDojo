71c5c9d7b3763da1bad32b3ab6d1a48753e3e239
likewhoa <likewhoa@weboperative.com>
2021-08-10 21:05:02 -0400

    Remove invalid variable change

--- Scripts/functions.sh
+++ Scripts/functions.sh
@@ -1245,9 +1245,8 @@ _mempool_conf() {
     # Enable mempool and set MySQL credentials
     sudo sed -i -e 's/MEMPOOL_INSTALL=.*$/MEMPOOL_INSTALL=on/' \
     -e "s/MYSQL_USER=.*$/MYSQL_USER=${MEMPOOL_MYSQL_USER}/" \
-    -e "s/MYSQL_ROOT_PASSWORD=.$/MYSQL_ROOT_PASSWORD=${MEMPOOL_MYSQL_ROOT_PASSWORD}/" \
-    -e "s/MYSQL_PASS=.*$/MYSQL_PASS=${MEMPOOL_MYSQL_PASSWORD}/" \
-    -e "s/MYSQL_PASSWORD=.*$/MYSQL_PASSWORD=${MEMPOOL_MYSQL_PASSWORD}/" "${dojo_path_my_dojo}"/conf/docker-mempool."${mempool_conf}"
+    -e "s/MYSQL_PASSWORD=.*$/MYSQL_PASSWORD=${MEMPOOL_MYSQL_PASSWORD}/" \
+    -e "s/MYSQL_ROOT_PASSWORD=.$/MYSQL_ROOT_PASSWORD=${MEMPOOL_MYSQL_ROOT_PASSWORD}/" "${dojo_path_my_dojo}"/conf/docker-mempool."${mempool_conf}"
 }
 
 #
